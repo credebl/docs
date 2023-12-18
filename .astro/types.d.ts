8f4417bc11474038ff0c88e84e7cd0bc4cc4dd57
karan <karan.tompe@ayanworks.com>
2023-12-18 15:06:09 +0530

    added error page
    Signed-off-by: karan <karan.tompe@ayanworks.com>
    Signed-off-by: pranalidhanavade <pranali.dhanavade@ayanworks.com>

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -185,6 +185,13 @@ declare module 'astro:content' {
 
 	type ContentEntryMap = {
 		"docs": {
+"404.md": {
+	id: "404.md";
+  slug: "404";
+  body: string;
+  collection: "docs";
+  data: InferEntrySchema<"docs">
+} & { render(): Render[".md"] };
 "en/guides/afj_setup.md": {
 	id: "en/guides/afj_setup.md";
   slug: "en/guides/afj_setup";
